aaf11890f1b931bb63e8a8bd74f7a875e1bfd1fb
manoj4909 <77528219+manoj4909@users.noreply.github.com>
2021-01-28 20:43:16 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -4,37 +4,17 @@
 
 using namespace Statistics;
 
-
-
 Stats Statistics::ComputeStatistics(const std::vector<float>& vect) {
-
-
-
 std::vector<float>::const_iterator itr = vect.begin();
-
 Stats S;
-
 float total = 0.0F;
-
 S.min = *std::min_element(vect.begin(),vect.end());
-
 S.max = *std::max_element(vect.begin(),vect.end());
-
-
-
 for( ; itr!=vect.end(); ++itr)
-
 {
-
 total = total + *itr;
-
 }
-
-
-
 S.average = total / vect.size() ;
-
 return S;
-
 }
 
