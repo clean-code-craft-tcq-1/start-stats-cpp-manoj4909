1f2b7f86972fce56422a775a31858058099b2aa3
manoj4909 <77528219+manoj4909@users.noreply.github.com>
2021-01-28 19:53:00 +0530

    initial code change for min max function

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,15 @@
 #include "stats.h"
-
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Statistics::Stats::ComputeStatistics(const std::vector<float>& values) 
+{
+    calc.c ={0.0F,0.0F,0.0F};
+	float total = 0.0F;
+	for(int i=0; i<values.size(); i++)
+    {   
+      total = total + values[i];
+    }
+    c.average = total/values.size();
+    c.minimum = *min_element(values.begin(),values.end());
+    c.maximum = *max_element(values.begin(),values.end());
+    return c;
     //Implement statistics here
 }
