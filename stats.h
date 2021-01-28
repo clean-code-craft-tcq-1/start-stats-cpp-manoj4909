3f05006b5a158903dd66913896d17c248c14e75c
manoj4909 <77528219+manoj4909@users.noreply.github.com>
2021-01-28 20:27:55 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,12 +2,22 @@
 
 namespace Statistics {
 
-	struct calc
-	{
-		float minimum;
-		float maximum;
-		float average;
-		
-	};
-    Stats ComputeStatistics(const std::vector<float>& values);
+struct Stats
+{
+float min;
+
+float max;
+
+float average;
+
+Stats()
+{
+min = 0.0f;
+max = 0.0f;
+average = 0.0f;
 }
+};
+
+Stats ComputeStatistics(const std::vector<float>& vect);
+}
+
